e743fe1dd4c26843a063631313a99b0297ef2294
Xueqiao Xu <xueqiaoxu@gmail.com>
2014-05-23 12:02:29 -0700

    add: skeleton

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var http = require('http');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+
+if ('development' === app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
