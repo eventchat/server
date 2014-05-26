f3246e85fd37a04aaf24b7b570a66e7cf597cfc7
Xueqiao Xu <xueqiaoxu@gmail.com>
2014-05-26 14:39:25 -0700

    mod: renamed app.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var http = require('http');
-var app = require('app/app');
+var app = require('./app');
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
