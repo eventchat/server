28ff472cd46e7788a795dbefbd29344c2978d69d
Xueqiao Xu <xueqiaoxu@gmail.com>
2014-05-26 15:55:32 -0700

    fix: don't show log in test

--- app/index.js
+++ app/index.js
@@ -7,7 +7,11 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.use(express.logger('dev'));
+
+if ('development' === app.get('env')) {
+  app.use(express.logger('dev'));
+}
+
 app.use(express.methodOverride());
 app.use(express.bodyParser());
 app.use(app.router);
