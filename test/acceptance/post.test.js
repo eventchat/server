5745cf1832ebfa159d21b28b5f80564c313d7b11
Xueqiao Xu <xueqiaoxu@gmail.com>
2014-05-27 16:04:36 -0700

    add: test for showing a single post

---
+++ test/acceptance/post.test.js
@@ -0,0 +1,80 @@
+/* globals describe, it, before, beforeEach, after */
+
+var mongoose = require('mongoose');
+var request  = require('supertest');
+var app      = require('../../app');
+var User     = require('../../app/models/user');
+var Event    = require('../../app/models/event');
+var Post     = require('../../app/models/post');
+var config   = require('../../config');
+
+
+describe('Post API', function () {
+
+  var user;
+  var event;
+  var post;
+
+  before(function () {
+    mongoose.connect(config.db.test.path);
+  });
+
+  beforeEach(function (done) {
+    // clear the database, then populate sample data
+    User.remove(function () {
+      Event.remove(function () {
+        Post.remove(function () {
+          user = new User({
+            name: 'Joe',
+            email: 'joe@example.com',
+            info: 'This guy is lazy',
+            password: '123456'
+          });
+          user.save(function () {
+            event = new Event({
+              name: 'PyCon',
+              description: 'Python Conference',
+              latitude: 37.3894,
+              longitude: -122.0819,
+            });
+            event.save(function () {
+              post = new Post({
+                type: 'text',
+                title: 'What\'s MetaClass',
+                body: 'Just dark magic',
+                event_id: String(event._id),
+                author_id: String(user._id)
+              });
+              post.save(done);
+            });
+          });
+        });
+      });
+    });
+  });
+
+  after(function () {
+    mongoose.disconnect();
+  });
+
+  describe('GET /posts/:post_id', function () {
+    it('it should respond with the post\'s json when the post exists', function (done) {
+      request(app)
+        .get('/posts/' + String(post._id))
+        .expect(function (res) {
+          res.status.should.eql(200);
+
+          res.body.should.have.properties({
+            id: String(post._id),
+            title: 'What\'s MetaClass',
+            body: 'Just dark magic',
+            author: user.toJSON(),
+            event: event.toJSON(),
+            comments: []
+          });
+          res.body.should.have.property('created_at');
+        })
+        .end(done);
+    });
+  });
+});
