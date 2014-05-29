0480bb19071c23a6e8317db0e487ec8e70fb0bc1
Xueqiao Xu <xueqiaoxu@gmail.com>
2014-05-29 13:16:23 -0700

    rm: redundant statements in config

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
 var env = process.env;
 
-console.log(env.EVENTCHAT_DB_URL);
-
 module.exports = {
-  DB_URL: env.MONGOHQ_URL || env.EVENTCHAT_DB_URL,
+  DB_URL: env.EVENTCHAT_DB_URL,
   SECRET: env.EVENTCHAT_SECRET
 };
