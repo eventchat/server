83b067508a6f781203c3463e184e88963c0fd75a
Xueqiao Xu <xueqiaoxu@gmail.com>
2014-08-05 16:41:03 -0700

    add: controller methods for confirming friendship

--- app/controllers/friendships.js
+++ app/controllers/friendships.js
@@ -1,9 +1,10 @@
-var async      = require('async');
-var User       = require('../models/user');
-var Friendship = require('../models/friendship');
+var async         = require('async');
+var User          = require('../models/user');
+var Notifications = require('./notifications');
+var Friendship    = require('../models/friendship');
 
 exports.index = function (req, res) {
-  var id = req.param.user_id;
+  var id = req.params.user_id;
 
   Friendship.find({
     $or: [
@@ -24,10 +25,53 @@ exports.index = function (req, res) {
     }, function (err, users) {
       if (err) { return res.send(400, { message: err }); }
 
-      res.json(users.map(function (u) { u.toJSON(); }));
+      res.json(users);
     });
   });
 };
 
 exports.create = function (req, res) {
+  var user = req.session.user;
+  if (!user) {
+    return res.send(401);
+  }
+
+  var id = req.params.user_id;
+
+  Friendship.find({
+    $or: [
+      { from: id, to: user.id }, 
+      { from: user.id, to: id },
+    ],
+  }, function (err, friendships) {
+    if (friendships.length === 0) {
+      // create friend request
+      Notifications.create(id, 'friend', JSON.stringify(user));
+
+      (new Friendship({
+        from: user.id,
+        to: id,
+        status: 'pending'
+      })).save(function () {
+        res.send(200);
+      });
+    } else if (friendships.length === 1) {
+      var f = friendships[0];
+      if (f.from === user.id) {
+        // friend request already sent
+        return res.send(200);
+      } else {
+        // acknowlege friendship request
+        Friendship.findByIdAndUpdate(f._id, {
+          $set: {
+            status: 'confirmed'
+          }
+        }, function () {
+          res.send(200);
+        });
+      }
+    } else {
+      console.error('should not reach here');
+    }
+  });
 };
